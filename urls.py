32d144434dfb3e6478b841d71e1ac4a95cd22124
Kevin Lin <kevinslin8@gmail.com>
2011-12-19 01:07:15 -0700

    initial commit

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'proto_django.views.home', name='home'),
+    # url(r'^proto_django/', include('proto_django.foo.urls')),
+
+    url(r'^bond/', direct_to_template, {'template':'bond.html'}),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
